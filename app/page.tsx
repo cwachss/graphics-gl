e645bd31e4f79eed8e0250148abe11b298ddc7cd
cwachss <chaniwachsstock@gmail.com>
2024-02-14 23:49:45 -0500

    start working on layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,93 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.scrollY);
+    // clean up code
+    document.documentElement.style.setProperty(
+      "--scrollPosition",
+      window.scrollY + ""
+    );
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className=" wrapper relative h-[100vh] w-full overflow-y-auto overflow-x-hidden scroll-smooth">
+      <header
+        id="home"
+        className="h-[100vh] bg-white grid p-6 place-content-center snap-start text-center gap-7"
+      >
+        <h1 className=" text-5xl leading-tight">Design by Chani Wachsstock</h1>
+        <h2 className="text-2xl">Freelance Graphic Design</h2>
+        <div className="grid place-content-center pt-28 ">
+          <svg
+            width="54"
+            height="31"
+            viewBox="0 0 54 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <path
+              d="M1.66745 1.25537L27.6963 30.2938M26.3421 30.2938L52.371 1.25537"
+              stroke="black"
+              stroke-width="2"
             />
-          </a>
+          </svg>
         </div>
-      </div>
+      </header>
+      {/* <section id="about" className="h-[100vh] snap-start">
+        About
+        <p>
+          Hi, I'm Chani Wachsstock; Software developer, project manager, and
+          graphic designer. <br />
+          Bachelor's in Computer Science from Thomas Edison University. <br />
+          Certified in graphic design through PS Design School.
+        </p>
+      </section> */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section id="gallery" className="h-[100vh] snap-start px-24 py-12 ">
+        <div className=" ">
+          <div className="flex flex-wrap gap-8 align-baseline pb-10 items-baseline">
+            <h2 className="text-5xl">Gallery</h2>
+            <div className="flex gap-8 text-base uppercase items-baseline">
+              <h3 className="  ">Cookbooks</h3>
+              <h3>Logos</h3>
+              <h3>Book Covers</h3>
+              <h3>Packaging</h3>
+              <h3>Brochures</h3>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-8 items-center">
+            <div className="grid grid-cols-3 gap-5 w-full *:rounded-md">
+              <div className=" bg-blue-200 text-gray-800 col-span-2 h-[250px]"></div>
+              <div className=" bg-blue-200 text-gray-800 h-[250px]"></div>
+              <div className=" bg-blue-200 text-gray-800 h-[250px]"></div>
+              <div className=" bg-blue-200 text-gray-800 col-span-2 h-[250px]"></div>
+            </div>
+            <div className=" grid gap-5">
+              <h4 className=" text-4xl">Layout Design</h4>
+              <p className="text-balance text-xl max-w-[300px]">
+                Custom cookbooks, brochures, and magazines
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="contact" className="h-[100vh] snap-start">
+        <form className="flex flex-col gap-4">
+          Contact Me
+          <input></input>
+          <input></input>
+          <textarea></textarea>
+        </form>
+      </section>
+      <div></div>
     </main>
   );
 }
